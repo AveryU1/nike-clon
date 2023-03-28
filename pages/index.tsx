18caf9d02d550c83d21a15361dbd426a509ac3e9
averyU1 <criss_19992016@outlook.com>
2023-03-28 17:22:00 -0400

    Configue environment with typescript and tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// import Head from "next/head";
+// import Image from "next/image";
+// import styles from "../styles/Home.module.css";
+import { NextPage } from 'next'
+
+const Home: NextPage = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">Bienvenidos a mi sitio web</h1>
+  </div>
+)
+
+export default Home
