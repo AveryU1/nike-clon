60d90056db301161332618621c8d0ea9dbebc0ab
Briggitte Merchan <briggitte>
2023-03-28 17:34:48 -0400

    create the navlinks

---
+++ constants/index.js
@@ -0,0 +1,64 @@
+export const NavLinks = [
+    {
+        title: 'New & Featured',
+        fields: {
+            title: 'New arrivals',
+            options: [
+                '100s of Styles Added',
+                'SNKRS Launch Calendar',
+                'New & Upcoming Drops'
+            ]
+        }
+    },
+    {
+        title: 'Man',
+        fields: {
+            title: 'All shoes',
+            options: [
+                'All Shoes',
+                'Lifestyle',
+                'Jordan',
+                'Air Max',
+                'Air Force 1',
+                'Blazer',
+                'Basketball',
+                'Running',
+                'Soccer'
+            ]
+        }
+    },
+    {
+        title: 'Woman',
+        fields: {
+            title: 'All Shoes',
+            options: [
+                'Lifestyle',
+                'Jordan',
+                'Air Max',
+                'Air Force 1',
+                'Dunks & Blazers',
+                'Running',
+                'Training & Gym',
+                'Basketball',
+                'Shoes $100 & Under'
+            ]
+        }
+    },
+    {
+        title: 'Kids',
+        fields: {
+            title: 'All Shoes',
+            options: [
+                'Big Kids (3.5Y - 7Y)',
+                'Little Kids (10.5C - 3Y)',
+                'Baby & Toddler (1C - 10C)',
+                'Jordan',
+                'Shoes $70 & Under',
+                'Lifestyle',
+                'Basketball',
+                'Running',
+                'Easy On & Off'
+            ]
+        }
+    }
+]
