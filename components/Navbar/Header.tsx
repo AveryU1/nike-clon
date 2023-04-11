a4936c84a92cc42985d8c35fd4200a5c8f80cb3a
Briggitte Merchan <briggitte>
2023-04-11 19:56:47 -0400

    navigation bar modifications

--- components/Navbar/Header.tsx
+++ components/Navbar/Header.tsx
@@ -14,29 +14,35 @@ const Header = () => {
   console.log(openMenu)
 
   return (
-    <nav className='bg-slate-100'>
-        <div className='flex items-center justify-between'>
-            <div>
+    <nav className='bg-slate-100 xs:overflow-hidden'>
+        <div className='flex items-center justify-around'>
+            <div className='z-[50] p-5 md:w-auto w-full flex justify-between'>
               <Link href='/'>
                   <img className='cursor-pointer h-9' src="/assets/nike-logo.png" alt="nike logo" />
               </Link>
+              
             </div>
             <div className='md:flex hidden items-center gap-8'>
                 <Navbar />
             </div>
-            <ul className='flex gap-4'>
+            <ul className='flex gap-4 xs:pr-8'>
               <li className='cursor-pointer'><BsSearch/></li>
               <li className='cursor-pointer md:flex hidden'><HiOutlineHeart/></li>
               <li className='cursor-pointer'><BsHandbag/></li>
               <div className='lg:hidden cursor-pointer' onClick={() => setOpenMenu(!openMenu)}>
-                <span className='cursor-pointer'>{ openMenu ? <GrClose/> : <AiOutlineMenu/>}</span>
+                <span className='cursor-pointer '>{ openMenu ? <GrClose/> : <AiOutlineMenu/>}</span>
               </div>
             </ul>
             
             {/* mobile nav */}
-            <div className={`md:hidden absolute w-full top-0 overflow-y-auto bottom-0 py-24 pl-4 duration-500 ${openMenu ? 'left-0' : 'left-[-100]'}`}>
+           { openMenu && 
+           (<div className={`md:hidden bg-slate-300 absolute bottom-0 right-0 w-full h-full py-12 pl-4 duration-500 ${openMenu ? 'left-0' : 'left-[100%]'}`}>
+            <div className='lg:hidden cursor-pointer p-5 flex justify-end' onClick={() => setOpenMenu(!openMenu)}>
+                <span className='cursor-pointer '>{ openMenu && <GrClose/> }</span>
+              </div>
               <Navbar/>
-            </div>
+            </div>)
+            }
         </div>
         
     </nav>
